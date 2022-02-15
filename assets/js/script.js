4d6cabdf8c7b05f78064fb2209029cfa04865f7b
Miguel Mulet <mmiguel032202@gmail.com>
2022-02-15 02:48:22 -0500

    Addition of Weather API & Functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,10 @@ var forecastTitle = document.querySelector("#forecast");
 var forecastContainerEl = document.querySelector("#fiveday-container");
 var pastSearchButtonEl = document.querySelector("#past-search-buttons");
 
-var formSumbitHandler = function(event){
+var formSumbitHandler = function(event) {
     event.preventDefault();
     var city = cityInputEl.value.trim();
-    
+
     if(city){
         getCityWeather(city);
         get5Day(city);
@@ -25,6 +25,54 @@ var formSumbitHandler = function(event){
     pastSearch(city);
 }
 
-var saveSearch = function(){
+var saveSearch = function() {
     localStorage.setItem("cities", JSON.stringify(cities));
 };
+
+var getCityWeather = function(city){
+    var apiKey= "844421298d794574c100e3409cee0499"
+    var apiURL= `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+    fetch(apiURL)
+    .then(function(response){
+        response.json().then(function(data){
+            displayWeather(data, city);
+        });
+    });
+};
+
+var displayWeather = function(weather, searchCity){
+
+   weatherContainerEl.textContent= "";  
+   citySearchInputEl.textContent=searchCity;
+
+   var currentDate = document.createElement("span")
+   currentDate.textContent=" (" + moment(weather.dt.value).format("MMM D, YYYY") + ") ";
+   citySearchInputEl.appendChild(currentDate);
+
+   var weatherIcon = document.createElement("img")
+   weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+   citySearchInputEl.appendChild(weatherIcon);
+
+   var temperatureEl = document.createElement("span");
+   temperatureEl.textContent = "Temperature: " + weather.main.temp + " °F";
+   temperatureEl.classList = "list-group-item"
+  
+   var humidityEl = document.createElement("span");
+   humidityEl.textContent = "Humidity: " + weather.main.humidity + " %";
+   humidityEl.classList = "list-group-item"
+
+   var windSpeedEl = document.createElement("span");
+   windSpeedEl.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
+   windSpeedEl.classList = "list-group-item"
+
+   weatherContainerEl.appendChild(temperatureEl);
+
+   weatherContainerEl.appendChild(humidityEl);
+
+   weatherContainerEl.appendChild(windSpeedEl);
+
+   var lat = weather.coord.lat;
+   var lon = weather.coord.lon;
+   getUvIndex(lat,lon)
+}
